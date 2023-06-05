efb8106a2d3efb876878f4bf8ec19982bda62ac6
Mali-zi <mne310872@yandex.ru>
2023-06-05 14:10:23 +0300

    fix: implement correct loading data from json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
 import './App.css';
 import etsy from './etsy.json';
+import Listing from './components/Listing';
 
-interface Data {
-  data: string;
+type Item = {
+  url: string,
+  title: string,
+  price: string,
+  quantity: number,
+  MainImage?: MainImage
+};
+
+type MainImage = {
+  listing_image_id: number,
+  hex_code: any,
+  red: any,
+  green: any,
+  blue: any,
+  hue: any,
+  saturation: any,
+  brightness: any,
+  is_black_and_white: any,
+  creation_tsz: any,
+  listing_id: number,
+  rank: any,
+  url_75x75: string,
+  url_170x135: string,
+  url_570xN: string,
+  url_fullxfull: string,
+  full_height: any,
+  full_width: any,
 };
 
 function App() {
-  let data: string = `${etsy}`;
-  let meetup = JSON.parse(data);
+  let items = JSON.parse(JSON.stringify(etsy));
+  const listItem = items.map((item: Item) => {
+    return (
+      <Listing 
+        url={item.url}
+        title={item.title}
+        price={item.price}
+        quantity={item.quantity}
+        mainImage={item.MainImage ? item.MainImage.url_570xN : "https://img1.etsystatic.com/156/0/12814579/il_570xN.1173240751_50hv.jpg"}
+      />
+    )
+  });
   return (
     <div>
-
+      {listItem}
     </div>
   );
 }
--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,26 +1,24 @@
-interface Data {
-  listing_id: number,
-  url: string,
-  MainImage: string,
-  title: string,
-  currency_code: string,
-  price: string,
-  quantity: number,
+type Data = {
+  url: string;
+  title: string;
+  price: string;
+  quantity: number;
+  mainImage: string;
 };
 
-function Listing({ items }: Data[]) {
+function Listing({ url, title, price, quantity, mainImage}: Data): JSX.Element {
   return (
     <div className="item-list">
       <div className="item">
         <div className="item-image">
-          <a href="https://www.etsy.com/listing/292754135/woodland-fairy">
-            <img src="https://img1.etsystatic.com/156/0/12814579/il_570xN.1173240751_50hv.jpg"></img>
+          <a href={url}>
+            <img alt={title} src={mainImage}></img>
           </a>
         </div>
         <div className="item-details">
-          <p className="item-title">Woodland Fairy</p>
-          <p className="item-price">$3.99</p>
-          <p className="item-quantity level-medium">12 left</p>
+          <p className="item-title">{title}</p>
+          <p className="item-price">{price}</p>
+          <p className="item-quantity level-medium">{quantity} left</p>
         </div>
       </div>
     </div>
